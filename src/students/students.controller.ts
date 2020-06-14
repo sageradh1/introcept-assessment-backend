5e85052f3ab432b5ff21441668358064452a5f37
Sagar <sageradh1@gmail.com>
2020-06-14 09:26:22 +0545

    Made basic controllers and its associated services

---
+++ src/students/students.controller.ts
@@ -0,0 +1,71 @@
+import { Controller,Post,Body,Get,Param,Patch,Delete, HttpException, HttpStatus, Req} from '@nestjs/common';
+import { StudentService } from './students.service';
+import { StudentDTO } from './dtos/student.dto';
+import { CommonResponse } from 'src/shared/common-response.model';
+import { GetStudentsDTO } from './dtos/getstudent.dto';
+
+
+// Controller for Student
+@Controller('api/student')
+export class StudentController {
+    constructor(private readonly studentsService: StudentService) {}
+
+    //#region 
+    //For assessment
+    @Post()
+    addStudent(@Body() reqbody : StudentDTO, @Req() req) : Promise<CommonResponse>{
+        
+        return this.studentsService.insertStudent(
+            reqbody,req.url, req.method,new Date().toISOString()
+        )
+    }
+
+    //for endpoint (api/student/all)
+    @Post('all')
+    getAllStudents(@Body() reqbody : GetStudentsDTO, @Req() req) : Promise<CommonResponse>{
+        return this.studentsService.getStudents(reqbody,req.url, req.method,new Date().toISOString());
+    }
+
+
+
+    //Extra endpoints for future
+    @Get(':id')
+    getStudent(
+        @Param('id') studentId: number,@Req() req
+        ) {
+            return this.studentsService.getSingleStudent(studentId,req.url, req.method,new Date().toISOString())
+        .then(
+            (result)=>{
+                if (result){
+                    return result;
+                }else {
+                    throw new HttpException('Student not found',HttpStatus.NOT_FOUND)
+                }
+            }
+        )
+        .catch(
+            ()=>{
+                throw new HttpException('Student not found catch',HttpStatus.NOT_FOUND)
+            }
+        );
+    }
+
+    @Patch(':id')
+    updateStudent(
+        @Param('id') prodId: string,
+        @Body() reqbody : StudentDTO,
+   
+    ) {
+        // TODO : for future 
+        return "update"
+    }
+
+    @Delete(':id')
+    removeStudent(
+        @Param('id') prodId: string
+        ) {
+        // TODO : for future 
+        return "delete"
+    }
+    //#endregion
+}
---
+++ src/students/students.service.ts
@@ -0,0 +1,61 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Student } from './interfaces/student.interface';
+import CustomDbModel from 'src/db/db.helper';
+import { StudentDTO } from './dtos/student.dto';
+import { CommonResponse } from '../shared/common-response.model';
+import { GetStudentsDTO } from './dtos/getstudent.dto';
+
+
+@Injectable()
+export class StudentService {
+
+  constructor (private readonly studentModel: CustomDbModel){}
+
+  private Students: Student[] = [];
+
+  async getSingleStudent(StudentId: number,path: string,method: string,date: string): Promise<CommonResponse> {
+    const student = await this.studentModel.getStudentById(StudentId);
+    
+    const response = new CommonResponse(
+      path, 
+      method,
+      200,
+      "Success",
+      date,
+      student
+    );
+    return response;
+  }
+
+  async insertStudent (student : StudentDTO, path: string,method: string,date: string ): Promise<CommonResponse>  {
+    const newStudent = await this.studentModel.addStudentToFile(student)
+    
+    const response = new CommonResponse(
+      path, 
+      method,
+      200,
+      "Success",
+      date,
+      newStudent
+    );
+    return response;
+  }
+
+  async getStudents (reqbody : GetStudentsDTO, path: string,method: string,date: string ): Promise<CommonResponse>  {
+    var students = []
+    try{
+      students = await this.studentModel.getListOfStudents(reqbody)
+      const response = new CommonResponse(
+        path, 
+        method,
+        200,
+        "Success",
+        date,
+        students
+      );
+        return response;
+      }catch{
+          throw new NotFoundException()
+      }
+  }
+}
