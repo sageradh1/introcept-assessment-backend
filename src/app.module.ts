814f1a25677f149590da2d3e2be2d511129284a7
Sagar <sageradh1@gmail.com>
2020-06-14 09:39:10 +0545

    Linked validation pipes and exception handlers to the APP module in global level

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { APP_FILTER, APP_PIPE } from '@nestjs/core';
 import { StudentModule } from './students/students.module';
+import { HttpExceptionFilter } from './exceptions/http-exception.filters';
+import { ValidationExceptionFilter } from './exceptions/validation-exception.filters';
+import { ValidationPipe } from './pipes/validation.pipes';
 
 @Module({
   imports: [StudentModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+  {
+    provide: APP_FILTER,
+    useClass: HttpExceptionFilter,
+  },
+  {
+    provide: APP_FILTER,
+    useClass: ValidationExceptionFilter,
+  },
+  {
+    provide: APP_PIPE,
+    useClass: ValidationPipe,
+  }
+  ],
 })
 export class AppModule {}
